aa3ae4b10e65949750f2da5c46f8fbd54ede8079
aleongithub <aleksandar.milojevic@gmail.com>
2015-11-07 22:18:52 +0100

    added current user to welcome message in header

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -1,5 +1,9 @@
 var app = angular.module('VibezApp', []);
 
-app.controller('HeaderController', function() {
-  this.foo = 'bar'
-});
+app.controller('HeaderController', ['$http', function($http) {
+  var _this = this;
+  $http.get('/session').success(function(data){
+    _this.current_user = data.current_user;
+    console.log(_this.current_user)
+  })
+}]);
