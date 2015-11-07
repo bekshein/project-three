e78fd0e194f4869d965ed7a8600d27cecc285de9
Rebekah Shein <rmshein@gmail.com>
2015-11-07 10:58:34 -0500

    updated User model with has_many passive_relationships and has_many followers associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,10 @@ class User < ActiveRecord::Base
     minimum: 4, allow_nil: true
   }
 
-  has_many :active_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def follow(other_user)
     active_relationships.create(followed_id: other_user.id)
