70ce830b8a6faee5749ad383c93b76ad2d7c3f44
Rebekah Shein <rmshein@gmail.com>
2015-11-07 19:30:23 -0500

    updated seeds.rb to create faker following relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,11 @@ User.create!(username: "foobs",
                email: email,
                password_digest: password_digest)
  end
+
+# Faker for following relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
