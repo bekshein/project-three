903325c7d3df5dd1d39e5463d3ca6e98ccf858f1
Rebekah Shein <rmshein@gmail.com>
2015-11-10 10:21:17 -0500

    updated post model with validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ActiveRecord::Base
   belongs_to :user, foreign_key: :user_id
+  # sets default order to newest to oldest
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :song_title, presence: true
+  validates :artist_name, presence: true
+  validates :vibe, presence: true
+
 end
