74773e789c5204a7086e4e6ad2caf2b3e743d494
Rebekah Shein <rmshein@gmail.com>
2015-11-10 10:20:45 -0500

    added create and destroy relationship method comments

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,6 +3,7 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(user)
+    # response to ajax requests, rails will call a .js.erb file wtih same name as the action
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
@@ -12,6 +13,7 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
+    # response to ajax requests, rails will call a .js.erb file wtih same name as the action
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
