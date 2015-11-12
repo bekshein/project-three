734a03bd3c3db01ba94d7318187b8284a1bf0fe0
Casey Lakritz <caseylakritz@Caseys-MacBook-Pro.local>
2015-11-12 14:39:01 -0500

    update angular.js to reflect api implementation

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -113,38 +113,6 @@ app.controller('PostsController', ['$http', '$scope', '$routeParams', function($
     })
   }
 
-    this.tracks = [];
-    this.searchString = "";
-    this.client_id = 'b5bc5954389edc0ac2a8c023851af762';
-
-
-    this.makeCall = function() {
-      console.log(this.searchString)
-      var ctrlObj = this;
-      var endpoint = 'http://api.soundcloud.com/tracks?q=';
-      endpoint += this.searchString + "&client_id=" + this.client_id;
-      var promise = $http.get(endpoint);
-      promise.success(function(data) {
-        console.log(data);
-        // console.log(angular.element('.play'));
-        ctrlObj.tracks = data;
-      });
-      promise.error(function(error){
-        console.log("error - " + error);
-      });
-    };
-    this.getPlayer = function() {
-      var url = "https://w.soundcloud.com/player/?url="
-      var el = angular.element(event.target)
-      var permalink_url = el.data('url');
-      var iframeId = el.data('id');
-      var iframe = angular.element('#iframe' + iframeId);
-      url = url + permalink_url;
-      iframe.attr('src', url);
-      iframe.attr('width', "500");
-      iframe.attr('height', "180");
-    };
-
   this.getPosts()
   this.getUserPosts();
 }]); // end of PostsController
